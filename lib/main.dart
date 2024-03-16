dbf0da18839347f6d05382b3ad7284390c207b2b
IsabellaDennis <belladennis326@gmail.com>
2024-03-16 15:28:11 +0530

    screens added

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,22 @@
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
+import 'package:qr_app/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Qr());
 }
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Qr extends StatefulWidget {
+  const Qr({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<Qr> createState() => _QrState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _QrState extends State<Qr> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        backgroundColor:Colors.teal,
-        body:Center(
-          child:Column(
-            mainAxisAlignment:MainAxisAlignment.center,
-            children: [
-              Text('Login',style:TextStyle(color:Colors.white,fontSize:25)),
-              SizedBox(height: 20,),
-              TextField(
-                decoration:InputDecoration(
-                  contentPadding:EdgeInsets.symmetric(vertical: 20.0),
-                  enabledBorder:OutlineInputBorder(
-                    borderSide:BorderSide(width:3,color:Colors.white),
-                  ),
-
-                  hintText:'Enter your Roll no',
-                  hintStyle:TextStyle(color:Colors.white),
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextField(
-                decoration:InputDecoration(
-                  contentPadding:EdgeInsets.symmetric(vertical: 20.0),
-                  enabledBorder:OutlineInputBorder(
-                    borderSide:BorderSide(width:3,color:Colors.white),
-                  ),
-
-                  hintText:'Enter your password',
-                  hintStyle:TextStyle(color:Colors.white),
-                )
-              ),
-              SizedBox(height: 25,),
-              TextButton(
-                style: TextButton.styleFrom(
-                  side: BorderSide(
-                    color: Colors.white,
-                  ),
-                  primary:Colors.white,
-                ),
-                onPressed: () {},
-                child: const Text('Login'),
-              ),
-              SizedBox(height: 30,),
-              TextButton(onPressed: (){}, child: Text('dont have account?Register',style:TextStyle(color:Colors.white),)),
-            ],
-          ),
-        ),
-      ),
+      home:MyApp(),
     );
   }
 }
-
-
